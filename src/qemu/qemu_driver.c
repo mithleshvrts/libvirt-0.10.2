50bb8604804c281a243f61191b853df03ac10240
Daniel P. Berrange <berrange@redhat.com>
2012-10-10 15:38:48 +0100

    Simplify some redundant locking while unref'ing objects
    https://bugzilla.redhat.com/show_bug.cgi?id=859712
    
    There is no need to hold the mutex when unref'ing
    virObject instances
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>
    (cherry picked from commit 7307c3c00c8e6814a4d413d7dace684e55dea5f8)

--- src/qemu/qemu_driver.c
+++ src/qemu/qemu_driver.c
@@ -3537,8 +3537,8 @@ endjob:
     ignore_value(qemuDomainObjEndAsyncJob(driver, wdEvent->vm));
 
 unlock:
-    if (virObjectUnref(wdEvent->vm))
-        virDomainObjUnlock(wdEvent->vm);
+    virDomainObjUnlock(wdEvent->vm);
+    virObjectUnref(wdEvent->vm);
     qemuDriverUnlock(driver);
     VIR_FREE(wdEvent);
 }
