c526817c9820cfb0d38f228ae215a9b000bc6af2
Daniel P. Berrange <berrange@redhat.com>
2012-10-11 19:56:48 -0600

    Fix QEMU test with 1.2.0 help output
    https://bugzilla.redhat.com/show_bug.cgi?id=863115
    
    The help output for QEMU 1.2.0 changed 'pci-assign' to 'kvm-pci-assign'.
    Since the new capabilities code does exact device name matching
    instead of substring matching, this caused the capabilities to go
    missing.
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>
    (cherry picked from commit 72574808d6ab06d7093f3695c0ba6e14616adea5)

--- src/qemu/qemu_capabilities.c
+++ src/qemu/qemu_capabilities.c
@@ -1334,6 +1334,8 @@ static struct qemuCapsObjectTypeProps qemuCapsObjectProps[] = {
       ARRAY_CARDINALITY(qemuCapsObjectPropsVirtioNet) },
     { "pci-assign", qemuCapsObjectPropsPciAssign,
       ARRAY_CARDINALITY(qemuCapsObjectPropsPciAssign) },
+    { "kvm-pci-assign", qemuCapsObjectPropsPciAssign,
+      ARRAY_CARDINALITY(qemuCapsObjectPropsPciAssign) },
     { "scsi-disk", qemuCapsObjectPropsScsiDisk,
       ARRAY_CARDINALITY(qemuCapsObjectPropsScsiDisk) },
     { "ide-drive", qemuCapsObjectPropsIDEDrive,
