5796b1915bd0e4f0843272c901c0ff9b1aa4441e
Eric Blake <eblake@redhat.com>
2012-10-19 21:54:43 -0600

    blockjob: implement shallow commit flag in qemu
    https://bugzilla.redhat.com/show_bug.cgi?id=772088
    
    Now that we can crawl the chain of backing files, we can do
    argument validation and implement the 'shallow' flag.  In
    testing this, I discovered that it can be handy to pass the
    shallow flag and an explicit base, as a means of validating
    that the base is indeed the file we expected.
    
    * src/qemu/qemu_driver.c (qemuDomainBlockCommit): Crawl through
    chain to implement shallow flag.
    * src/libvirt.c (virDomainBlockCommit): Relax API.
    (cherry picked from commit 0a220e22251fbb047791f50fd2d15b49f274fc07)

--- src/libvirt.c
+++ src/libvirt.c
@@ -19376,8 +19376,6 @@ int virDomainBlockCommit(virDomainPtr dom, const char *disk,
     }
 
     virCheckNonNullArgGoto(disk, error);
-    if (flags & VIR_DOMAIN_BLOCK_COMMIT_SHALLOW)
-        virCheckNullArgGoto(base, error);
 
     if (conn->driver->domainBlockCommit) {
         int ret;
