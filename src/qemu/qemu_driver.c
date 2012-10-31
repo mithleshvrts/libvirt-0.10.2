7caed96ceb9b424e998f2c2d3243853cc8613539
Martin Kletzander <mkletzan@redhat.com>
2012-10-31 16:29:00 +0100

    qemu: Fix EmulatorPinInfo without emulatorpin
    https://bugzilla.redhat.com/show_bug.cgi?id=871312
    
    Recent fixes made almost all the right steps to make emulator pinned
    to the cpuset of the whole domain in case <emulatorpin> isn't
    specified, but qemudDomainGetEmulatorPinInfo still reports all the
    CPUs even when cpuset is specified.  This patch fixes that.
    (cherry picked from commit 10c5212b108e8395a6cab2dd449f2b1c0f1442d0)

--- src/qemu/qemu_driver.c
+++ src/qemu/qemu_driver.c
@@ -4345,7 +4345,6 @@ qemudDomainGetEmulatorPinInfo(virDomainPtr dom,
     virDomainDefPtr targetDef = NULL;
     int ret = -1;
     int maxcpu, hostcpus, pcpu;
-    virDomainVcpuPinDefPtr emulatorpin = NULL;
     virBitmapPtr cpumask = NULL;
     bool pinned;
 
@@ -4387,14 +4386,15 @@ qemudDomainGetEmulatorPinInfo(virDomainPtr dom,
         cpumaps[maplen - 1] &= (1 << maxcpu % 8) - 1;
     }
 
-    /* If no emulatorpin, all cpus should be used */
-    emulatorpin = targetDef->cputune.emulatorpin;
-    if (!emulatorpin) {
+    if (targetDef->cputune.emulatorpin) {
+        cpumask = targetDef->cputune.emulatorpin->cpumask;
+    } else if (targetDef->cpumask) {
+        cpumask = targetDef->cpumask;
+    } else {
         ret = 0;
         goto cleanup;
     }
 
-    cpumask = emulatorpin->cpumask;
     for (pcpu = 0; pcpu < maxcpu; pcpu++) {
         if (virBitmapGetBit(cpumask, pcpu, &pinned) < 0)
             goto cleanup;
