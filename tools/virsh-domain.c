c6fa5784e5533842c5b292d95f25120dd6260d5a
Ján Tomko <jtomko@redhat.com>
2012-12-03 15:20:19 +0100

    virsh: use correct sizeof when allocating cpumap
    Bug: https://bugzilla.redhat.com/show_bug.cgi?id=878376
    
    Found by coverity:
    Error: SIZEOF_MISMATCH (CWE-569):
        libvirt-0.10.2/tools/virsh-domain.c:4754: suspicious_sizeof: Passing
        argument "8UL /* sizeof (cpumap) */" to function
        "_vshCalloc(vshControl *, size_t, size_t, char const *, int)" and
        then casting the return value to "unsigned char *" is suspicious.
    
    Error: SIZEOF_MISMATCH (CWE-569):
        libvirt-0.10.2/tools/virsh-domain.c:4942: suspicious_sizeof: Passing
        argument "8UL /* sizeof (cpumap) */" to function
        "_vshCalloc(vshControl *, size_t, size_t, char const *, int)" and
        then casting the return value to "unsigned char *" is suspicious.
    (cherry picked from commit dc04b2a737c8c4240d0d412080ce798b977068a1)

--- tools/virsh-domain.c
+++ tools/virsh-domain.c
@@ -4751,7 +4751,7 @@ cmdVcpuPin(vshControl *ctl, const vshCmd *cmd)
 
     /* Pin mode: pinning specified vcpu to specified physical cpus*/
 
-    cpumap = vshCalloc(ctl, cpumaplen, sizeof(cpumap));
+    cpumap = vshCalloc(ctl, cpumaplen, sizeof(*cpumap));
     /* Parse cpulist */
     cur = cpulist;
     if (*cur == 0) {
@@ -4939,7 +4939,7 @@ cmdEmulatorPin(vshControl *ctl, const vshCmd *cmd)
 
     /* Pin mode: pinning emulator threads to specified physical cpus*/
 
-    cpumap = vshCalloc(ctl, cpumaplen, sizeof(cpumap));
+    cpumap = vshCalloc(ctl, cpumaplen, sizeof(*cpumap));
     /* Parse cpulist */
     cur = cpulist;
     if (*cur == 0) {
