c68321f67c50ee62978207e538e28204532f5946
Peter Krempa <pkrempa@redhat.com>
2013-06-13 15:48:10 +0200

    qemu: Remove maximum cpu limit when setting processor count using the API
    https://bugzilla.redhat.com/show_bug.cgi?id=855296
    
    When setting processor count for a domain using the API libvirt enforced
    a maximum processor count, while it isn't enforced when taking the XML path.
    
    This patch removes the check to match the XML.
    (cherry picked from commit ce65b435899066243c303dd1cefd671de46ae592)
    
    Conflicts:
    	src/qemu/qemu_driver.c: context

--- src/qemu/qemu_driver.c
+++ src/qemu/qemu_driver.c
@@ -3925,8 +3925,6 @@ qemuDomainSetVcpusFlags(virDomainPtr dom, unsigned int nvcpus,
     struct qemud_driver *driver = dom->conn->privateData;
     virDomainObjPtr vm;
     virDomainDefPtr persistentDef;
-    const char * type;
-    int max;
     int ret = -1;
     bool maximum;
 
@@ -3969,27 +3967,11 @@ qemuDomainSetVcpusFlags(virDomainPtr dom, unsigned int nvcpus,
         goto endjob;
     }
 
-    if (!(type = virDomainVirtTypeToString(vm->def->virtType))) {
-        virReportError(VIR_ERR_INTERNAL_ERROR,
-                       _("unknown virt type in domain definition '%d'"),
-                       vm->def->virtType);
-        goto endjob;
-    }
-
-    if ((max = qemudGetMaxVCPUs(NULL, type)) < 0) {
-        virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
-                       _("could not determine max vcpus for the domain"));
-        goto endjob;
-    }
-
-    if (!maximum && vm->def->maxvcpus < max) {
-        max = vm->def->maxvcpus;
-    }
-
-    if (nvcpus > max) {
+    if (!maximum && nvcpus > vm->def->maxvcpus) {
         virReportError(VIR_ERR_INVALID_ARG,
                        _("requested vcpus is greater than max allowable"
-                         " vcpus for the domain: %d > %d"), nvcpus, max);
+                         " vcpus for the domain: %d > %d"),
+                       nvcpus, vm->def->maxvcpus);
         goto endjob;
     }
 
