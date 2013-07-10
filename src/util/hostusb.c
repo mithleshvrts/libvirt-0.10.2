82605e549331e95e33e03ed2504d73c000f689c1
Martin Kletzander <mkletzan@redhat.com>
2013-07-10 16:59:02 +0200

    usb: don't spoil decimal addresses
    https://bugzilla.redhat.com/show_bug.cgi?id=981503
    
    For USB devices, dev->name gets formated as %.3o:%.3o even though the
    numbers are decimal.
    
    Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=967712
    (cherry picked from commit dc0c7915697a902d6edc5820f5930653b7684c3c)

--- src/util/hostusb.c
+++ src/util/hostusb.c
@@ -313,10 +313,10 @@ usbGetDevice(unsigned int bus,
     dev->bus     = bus;
     dev->dev     = devno;
 
-    if (snprintf(dev->name, sizeof(dev->name), "%.3o:%.3o",
+    if (snprintf(dev->name, sizeof(dev->name), "%.3d:%.3d",
                  dev->bus, dev->dev) >= sizeof(dev->name)) {
         virReportError(VIR_ERR_INTERNAL_ERROR,
-                       _("dev->name buffer overflow: %.3o:%.3o"),
+                       _("dev->name buffer overflow: %.3d:%.3d"),
                        dev->bus, dev->dev);
         usbFreeDevice(dev);
         return NULL;
