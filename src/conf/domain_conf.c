libvirt/libvirt

Add support for locking domain's memory pages
The following XML configuration can be used to request all domain's
memory pages to be kept locked in host's memory (i.e., domain's memory
pages will not be swapped out):

      <memoryBacking>
        <locked/>
      </memoryBacking>

src/conf/domain_conf.c
@@ -10411,6 +10411,9 @@ virDomainDefParseXML(xmlDocPtr xml,
     if ((node = virXPathNode("./memoryBacking/nosharepages", ctxt)))
         def->mem.nosharepages = true;
 
+    if (virXPathBoolean("boolean(./memoryBacking/locked)", ctxt))
+        def->mem.locked = true;
+
     /* Extract blkio cgroup tunables */
     if (virXPathUInt("string(./blkiotune/weight)", ctxt,
                      &def->blkio.weight) < 0)
@@ -15766,17 +15769,16 @@ virDomainDefFormatInternal(virDomainDefPtr def,
         def->mem.swap_hard_limit)
         virBufferAddLit(buf, "  </memtune>\n");
 
-    if (def->mem.hugepage_backed || def->mem.nosharepages)
+    if (def->mem.hugepage_backed || def->mem.nosharepages || def->mem.locked) {
         virBufferAddLit(buf, "  <memoryBacking>\n");
-
-    if (def->mem.hugepage_backed)
-        virBufferAddLit(buf, "    <hugepages/>\n");
-
-    if (def->mem.nosharepages)
-        virBufferAddLit(buf, "    <nosharepages/>\n");
-
-    if (def->mem.hugepage_backed || def->mem.nosharepages)
+        if (def->mem.hugepage_backed)
+            virBufferAddLit(buf, "    <hugepages/>\n");
+        if (def->mem.nosharepages)
+            virBufferAddLit(buf, "    <nosharepages/>\n");
+        if (def->mem.locked)
+            virBufferAddLit(buf, "    <locked/>\n");
         virBufferAddLit(buf, "  </memoryBacking>\n");
+    }
 
     virBufferAddLit(buf, "  <vcpu");
     virBufferAsprintf(buf, " placement='%s'",

mithleshvrts/libvirt-0.10.2

https://bugzilla.redhat.com/show_bug.cgi?id=947118
(cherry picked from commit 555866389c26a1bcf4f6cec9406e3c8ad435d778)
Conflicts:
	docs/formatdomain.html.in - context and removal of unrelated
	    blkiotune crap that went in 89e0e17 RHEL-only patch
	docs/schemas/domaincommon.rng - context
	src/conf/domain_conf.c, src/conf/domain_conf.h - context:
	    mem.ksm_disabled vs. mem.nosharepages

src/conf/domain_conf.c
@@ -8850,6 +8850,9 @@ static virDomainDefPtr virDomainDefParseXML(virCapsPtr caps,
     if (node)
         def->mem.ksm_disabled = 1;
 
+    if (virXPathBoolean("boolean(./memoryBacking/locked)", ctxt))
+        def->mem.locked = true;
+
     /* Extract blkio cgroup tunables */
     if (virXPathUInt("string(./blkiotune/weight)", ctxt,
                      &def->blkio.weight) < 0)
@@ -13867,17 +13870,16 @@ virDomainDefFormatInternal(virDomainDefPtr def,
         def->mem.swap_hard_limit)
         virBufferAddLit(buf, "  </memtune>\n");
 
-    if (def->mem.hugepage_backed || def->mem.ksm_disabled)
+    if (def->mem.hugepage_backed || def->mem.ksm_disabled || def->mem.locked) {
         virBufferAddLit(buf, "  <memoryBacking>\n");
-
-    if (def->mem.hugepage_backed)
-        virBufferAddLit(buf, "    <hugepages/>\n");
-
-    if (def->mem.ksm_disabled)
-        virBufferAddLit(buf, "    <nosharepages/>\n");
-
-    if (def->mem.hugepage_backed || def->mem.ksm_disabled)
+        if (def->mem.hugepage_backed)
+            virBufferAddLit(buf, "    <hugepages/>\n");
+        if (def->mem.ksm_disabled)
+            virBufferAddLit(buf, "    <nosharepages/>\n");
+        if (def->mem.locked)
+            virBufferAddLit(buf, "    <locked/>\n");
         virBufferAddLit(buf, "  </memoryBacking>\n");
+    }
 
     virBufferAddLit(buf, "  <vcpu");
     virBufferAsprintf(buf, " placement='%s'",