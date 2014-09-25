libvirt/libvirt

qemu: Implement support for locking domain's memory pages

src/qemu/qemu_command.c
@@ -6541,6 +6541,17 @@ qemuBuildCommandLine(virConnectPtr conn,
                              cfg->hugepagePath, NULL);
     }
 
+    if (def->mem.locked && !virQEMUCapsGet(qemuCaps, QEMU_CAPS_MLOCK)) {
+        virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
+                       _("memory locking not supported by QEMU binary"));
+        goto error;
+    }
+    if (virQEMUCapsGet(qemuCaps, QEMU_CAPS_MLOCK)) {
+        virCommandAddArg(cmd, "-realtime");
+        virCommandAddArgFormat(cmd, "mlock=%s",
+                               def->mem.locked ? "on" : "off");
+    }
+
     virCommandAddArg(cmd, "-smp");
     if (!(smp = qemuBuildSmpArgStr(def, qemuCaps)))
         goto error;

mithleshvrts/libvirt-0.10.2

https://bugzilla.redhat.com/show_bug.cgi?id=947118
(cherry picked from commit fd74f74fe609f3e3eb33ef70fb6a2d402c4cd519)
Conflicts:
	src/qemu/qemu_capabilities.c - no QMP probing in RHEL-6
	src/qemu/qemu_capabilities.h - context
        src/qemu/qemu_command.c - s/virQEMUCapsGet/qemuCapsGet/ and
	    s/qemuCaps/caps/
	tests/qemuxml2argvtest.c - context
        tests/qemuxml2argvdata/qemuxml2argv-*.args - reorder -usb

src/qemu/qemu_command.c
@@ -5018,6 +5018,17 @@ qemuBuildCommandLine(virConnectPtr conn,
         virCommandAddArg(cmd, "-redhat-disable-KSM");
     }
 
+    if (def->mem.locked && !qemuCapsGet(caps, QEMU_CAPS_MLOCK)) {
+        virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
+                       _("memory locking not supported by QEMU binary"));
+        goto error;
+    }
+    if (qemuCapsGet(caps, QEMU_CAPS_MLOCK)) {
+        virCommandAddArg(cmd, "-realtime");
+        virCommandAddArgFormat(cmd, "mlock=%s",
+                               def->mem.locked ? "on" : "off");
+    }
+
     virCommandAddArg(cmd, "-smp");
     if (!(smp = qemuBuildSmpArgStr(def, caps)))
         goto error;